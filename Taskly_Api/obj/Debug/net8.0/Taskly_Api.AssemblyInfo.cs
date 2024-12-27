7c0a5779ad43e62e7f6d465029e77ad268720a29
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-12-27 11:47:00 +0200

    add avatars

--- Taskly_Api/obj/Debug/net8.0/Taskly_Api.AssemblyInfo.cs
+++ Taskly_Api/obj/Debug/net8.0/Taskly_Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taskly_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8700456be0eab1f57e9ccbea1041c2564ce347ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taskly_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taskly_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
