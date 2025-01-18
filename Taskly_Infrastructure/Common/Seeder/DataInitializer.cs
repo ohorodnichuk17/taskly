d1bf687d47dfcbcf5dc49acfb9c064293f62327e
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-01-18 11:28:08 +0200

    add save changes to InitializeDashboardTemplatesAsync method

--- Taskly_Infrastructure/Common/Seeder/DataInitializer.cs
+++ Taskly_Infrastructure/Common/Seeder/DataInitializer.cs
@@ -63,6 +63,7 @@ public static class DataInitializer
             };
 
             await dbContext.BoardTemplates.AddRangeAsync(boardTemplates);
+            await dbContext.SaveChangesAsync();
         }
     }
     
