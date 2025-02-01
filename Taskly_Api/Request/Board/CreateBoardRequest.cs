eb76388afad4e038939cebe7e048d9a9dd2722ac
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-02-01 18:24:44 +0200

    userId while creating board | error

--- Taskly_Api/Request/Board/CreateBoardRequest.cs
+++ Taskly_Api/Request/Board/CreateBoardRequest.cs
@@ -1,6 +1,7 @@
 namespace Taskly_Api.Request.Board;
 
 public record CreateBoardRequest(
+    Guid UserId,
     string Name,
     string? Tag,
     bool IsTeamBoard,
--- Taskly_Application/Requests/Board/Command/CreateBoard/CreateBoardCommandHandler.cs
+++ Taskly_Application/Requests/Board/Command/CreateBoard/CreateBoardCommandHandler.cs
@@ -27,6 +27,7 @@ public class CreateBoardCommandHandler(IUnitOfWork unitOfWork,
 
             var board = new BoardEntity
             {
+                Id = Guid.NewGuid(),
                 Name = request.Name,
                 Tag = request.Tag,
                 IsTeamBoard = request.IsTeamBoard,
@@ -34,9 +35,11 @@ public class CreateBoardCommandHandler(IUnitOfWork unitOfWork,
                 CardLists = cardLists
             };
             
-            var result = await unitOfWork.Board.CreateAsync(board);
+            var createdBoard = await unitOfWork.Board.CreateAsync(board);
+            
+            await unitOfWork.Board.AddMemberToBoardAsync(createdBoard, request.UserId);
 
-            return result;
+            return createdBoard;
         }
         catch (Exception ex)
         {
