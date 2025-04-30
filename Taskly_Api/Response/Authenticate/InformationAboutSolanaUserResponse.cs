233bd2161ca7ef84b47f354d01b9e7e99191a88c
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-04-30 15:32:48 +0300

    add mapping for solana user

--- Taskly_Api/Response/Authenticate/InformationAboutSolanaUserResponse.cs
+++ Taskly_Api/Response/Authenticate/InformationAboutSolanaUserResponse.cs
@@ -1,8 +1,8 @@
-namespace Taskly_Api.Request.Authenticate;
+namespace Taskly_Api.Response.Authenticate;
 
 public class InformationAboutSolanaUserResponse
 {
     public Guid Id { get; set; }
     public required string PublicKey { get; set; }
-    public required Guid AvatarId { get; set; }
+    public required string AvatarName { get; set; }
 }
