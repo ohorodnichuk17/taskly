14fb73e19a395c02154f9cbca98e7551f71569b4
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-05-13 16:40:08 +0300

    modify cors policy

--- Taskly_Api/DependencyInjection.cs
+++ Taskly_Api/DependencyInjection.cs
@@ -238,7 +238,7 @@ public static class DependencyInjection
             options.AddPolicy("AllowPolicy", policy =>
             {
 
-                policy.WithOrigins("https://taskly-frontend-5bz1.onrender.com")
+                policy.WithOrigins("https://taskly-frontend-5bz1.onrender.com", "http://localhost:5173")
                     .AllowCredentials() // ����� �� ����-�� ������ ���
                     .AllowAnyMethod() // ����� �� ����-�� ������
                     .AllowAnyHeader(); //����� �� ����-�� �������� ���
