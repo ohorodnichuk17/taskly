42013c79e161e111d2c496ca2bd1b0cf0abd5b61
zubnaz <nzubar4@gmail.com>
2025-05-20 15:09:13 +0300

    Delete unnecessary files

--- TasklyServer_Application/Requests/Command/SendMail/SendMailCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace TasklyServer_Application.Requests.Command.SendMail;
-
-public record SendMailCommand(string Subject, string To, string Body, bool IsBodyHtml, Dictionary<string, string> Props) : IRequest;
